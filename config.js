8efff384bad587876f97219f65bb6c926ff7fd43
Aiko-Danna <89666379+Aiko-Danna@users.noreply.github.com>
2022-08-27 10:38:08 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCp3bwztSY4qtFh9WOcSgN0d65NFJWnxqY",
+  authDomain: "app-narrar-historias-8e356.firebaseapp.com",
+  projectId: "app-narrar-historias-8e356",
+  storageBucket: "app-narrar-historias-8e356.appspot.com",
+  messagingSenderId: "492927508605",
+  appId: "1:492927508605:web:b3305f92a68cafae449114"
+};
---
+++ screens/Logout.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar, Image, Switch } from "react-native";
+import firebase from 'firebase';
+
+export default class Logout extends Component {
+  
+  componentDidMount(){
+    firebase.auth().signOut();
+  }
+
+  render(){
+    return(
+      <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+        <Text> Cerrar sesión </Text>
+      </View>
+    );
+  }
+}
